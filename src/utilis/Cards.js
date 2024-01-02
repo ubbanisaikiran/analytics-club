e725aada2c9ec3dce778591db857650e742aeef2
saikiran <saikiranubbani@gmail.com>
2024-01-02 17:45:44 +0530

    image & video changes

--- src/utilis/Cards.js
+++ src/utilis/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Card.css';
 import CardItem from './CardItem';
+import img1 from '../images/python.png'
+import img2 from '../images/saas.jpg'
+import img3 from '../images/figma.png'
+import img4 from '../images/sql.jpg'
+import img5 from '../images/blockchain.jpg'
 
 function Cards() {
   return (
@@ -10,13 +15,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/python.png'
+              src={img1}
               text='Explore the python features helps for quick and fast Anlaysis of Data'
               label='Python'
               path='/Courses/material'
             />
             <CardItem
-              src='images/saas.jpg'
+              src={img2}
               text='SaaS is a cloud-based software distribution model in which applications are hosted and maintained '
               label='SaaS'
               path='/Courses/material'
@@ -24,19 +29,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/figma.png'
+              src={img3}
               text='Design and prototyping tool used by designers to create and share user interfaces and interactive designs'
               label='Figma'
               path='/Courses/material'
             />
             <CardItem
-              src='images/sql.jpg'
+              src={img4}
               text='SQL  is a domain-specific language used for managing and manipulating relational databases'
               label='SQL'
               path='/Courses/material'
             />
             <CardItem
-              src='images/blockchain.jpg'
+              src={img5}
               text='Blockchain is a decentralized, distributed ledger technology that records transactions across multiple computers.'
               label='Block chain'
               path='/courses/material'
--- src/utilis/HeroSection.js
+++ src/utilis/HeroSection.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../App.css';
 import { Button } from '../components/Button';
 import './HeroSection.css';
+import videoSource from '../videos/video-1.mp4';
+
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+    <video src={videoSource} autoPlay loop muted type="video/mp4" />
       <h1>QUAERERE - Analytics Club</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
